cce8cc65b2b18644c681a7619683481014a0e8b1
xiechongyang <xiechongyang7@gmail.com>
2019-08-06 15:24:31 +0800

    提交

--- mez/src/main/java/demo/Example.java
+++ mez/src/main/java/demo/Example.java
@@ -16,31 +16,18 @@ import java.util.Scanner;
  */
 public class Example {
 
+
+
+
     public static void main(String[] arg) throws Exception {
-        System.out.println("*********************start");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入文件位置：");
-        String filePath = sc.nextLine();
-
-        File file = new File(filePath);
-        List<String> strList;
-
-        try {
-            strList = FileUtils.readLines(file, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new Exception("文件读取出错：");
-        }
-
-        if (strList.isEmpty()) {
-            throw new Exception("没有读取到有效信息");
-        }
-
+        String filePath = input();
+        List<String> strList = getStrLine(filePath);
+        List<String> list = new ArrayList<>();
+        List<String> getList = new ArrayList<>();
+        List<String> setList = new ArrayList<>();
         String className = "className";
-//        List<String> setList = new ArrayList<>();
-//        List<String> getList = new ArrayList<>();
-        int i;
-        for (i = 0; i < strList.size(); i++) {
+
+        for (int i = 0; i < strList.size(); i++) {
             String line = strList.get(i);
             if (!isNullStr(line)) {
                 if(line.contains("get")&&line.contains("(")){
@@ -50,15 +37,14 @@ public class Example {
                         || line.contains("import")
                         || line.contains("/")
                         || line.contains("@")
-                        || line.contains("*")
-                ) {
+                        || line.contains("*")) {
                     continue;
                 }
 
                 if (line.contains("public")) {
                     if (line.contains("class")) {
                         className = line.split(" ")[2];
-                        className = className.substring(0, 1).toLowerCase() + className.substring(1);
+                        className = className.substring(0, 1).toUpperCase() + className.substring(1);
                         continue;
                     } else {
                         break;
@@ -75,50 +61,77 @@ public class Example {
                     continue;
                 }
 
-//                if (line.contains("boolean") || line.contains("Boolean")) {
-//                    String fieldName = value.substring(0, 1).toUpperCase() + value.substring(1);
-//                    String getMethodName = className + ".is" + fieldName + "();";
-//                    getList.add(getMethodName);
-//                    StringBuilder setMethodName = new StringBuilder(className);
-//                    setMethodName.append(".set");
-//                    setMethodName.append(fieldName);
-//                    setMethodName.append("(");
-//                    setMethodName.append(value);
-//                    setMethodName.append(");");
-//                    setList.add(String.valueOf(setMethodName));
-//                    continue;
-//                }
-
-
                 String fieldName = value.substring(0, 1).toUpperCase() + value.substring(1);
+                getAndSet(line,className,value,fieldName,getList,setList);
+                String printStr =  getExample(className,value,fieldName);
+                list.add(printStr);
 
-                StringBuilder setMethodName = new StringBuilder(className);
-
-
-//                String getMethodName = className + ".get" + fieldName + "();";
-//                getList.add(getMethodName);
-//                StringBuilder setMethodName = new StringBuilder(className);
-//                setMethodName.append(".set");
-//                setMethodName.append(fieldName);
-//                setMethodName.append("(");
-//                setMethodName.append(value);
-//                setMethodName.append(");");
-//                setList.add(String.valueOf(setMethodName));
             }
         }
 
         System.out.println("---------------------------start");
-//        for (String str : setList) {
-//            System.out.println(str);
-//        }
-//        System.out.println("---------------------------get");
-//        for (String str : getList) {
-//            System.out.println(str);
-//        }
+        System.out.println("---------------------------set");
+        for (String str : setList) {
+            System.out.println(str);
+        }
+        System.out.println("---------------------------get");
+
+        System.out.println("Integer startPageNum = accountAccounts.getStartPageNum();\n" +
+                "Integer endPageNum = accountAccounts.getEndPageNum();\n" +
+                "Date starTime = accountAccounts.getStarTime();\n" +
+                "Date endTime = accountAccounts.getEndTime();");
+        for (String str : getList) {
+            System.out.println(str);
+        }
+//        System.out.println("*********************");
+        for (String str : list) {
+            System.out.println(str);
+        }
+        System.out.println("if(isNotNull(starTime)){criteria.andCreateTimeGreaterThanOrEqualTo(starTime);}\n" +
+                "if(isNotNull(endTime)){criteria.andCreateTimeLessThanOrEqualTo(endTime);}\n" +
+                "if(isNotNull(startPageNum)){example.setLimitStart(startPageNum);}\n" +
+                "if(isNotNull(endPageNum)){example.setLimitEnd(endPageNum);}\n" +
+                "return example;");
         System.out.println("*********************end");
     }
 
 
+
+    /**
+     * 获取文件位置
+     * @return
+     */
+    private static String input(){
+        System.out.println("*********************start");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入文件位置：");
+        String filePath = sc.nextLine();
+        return filePath;
+    }
+
+    /**
+     * 获取文件字符串行
+     * @param filePath
+     * @return
+     */
+    private static List<String> getStrLine(String filePath) throws Exception {
+        File file = new File(filePath);
+        List<String> strList;
+
+        try {
+            strList = FileUtils.readLines(file, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new Exception("文件读取出错：");
+        }
+
+        if (strList.isEmpty()) {
+            throw new Exception("没有读取到有效信息");
+        }
+
+        return strList;
+
+    }
     /**
      * 判断是否为空
      *
@@ -129,5 +142,66 @@ public class Example {
         return str.length() == 0 || "".equals(str) || str == null;
     }
 
+    static boolean flag = true;
+    private static String getExample(final String className,String value,String fieldName){
+        StringBuilder builder = new StringBuilder();
+        /**
+         * 		AccountAccountsExample example = new AccountAccountsExample();
+         * 		AccountAccountsExample.Criteria criteria = example.createCriteria();
+         */
+        if(flag){
+            builder = new StringBuilder(className);
+            builder.append("Example example = new ");
+            builder.append(className);
+            builder.append("Example();\n");
+            builder.append(className);
+            builder.append("Example.Criteria criteria = example.createCriteria();");
+            builder.append("\n");
+            flag = false;
+        }
 
+
+        /**
+         * 		if(isNotNull(accountId)){criteria.andAccountIdEqualTo(accountId);}
+         */
+        builder.append("if(isNotNull(");
+        builder.append(value);
+        builder.append(")){criteria.and");
+        builder.append(fieldName);
+        builder.append("EqualTo(");
+        builder.append(value);
+        builder.append(");}");
+        return builder.toString();
+    }
+
+    private static void getAndSet(String line,final String classNames,String value,String fieldName,List<String> getList,List<String> setList){
+
+        String className = classNames.substring(0, 1).toLowerCase() + classNames.substring(1);
+        String[] types = line.split(" ");
+        String type  = types[types.length-2];
+
+        if (line.contains("boolean") || line.contains("Boolean")) {
+            String getMethodName = type +" "+ value + " = "+className + ".is" + fieldName + "();";
+            getList.add(getMethodName);
+            StringBuilder setMethodName = new StringBuilder(className);
+            setMethodName.append(".set");
+            setMethodName.append(fieldName);
+            setMethodName.append("(");
+            setMethodName.append(value);
+            setMethodName.append(");");
+            setList.add(String.valueOf(setMethodName));
+            return;
+        }
+
+
+        String getMethodName = type + " " + value + " = "+className + ".get" + fieldName + "();";
+        getList.add(getMethodName);
+        StringBuilder setMethodName = new StringBuilder(className);
+        setMethodName.append(".set");
+        setMethodName.append(fieldName);
+        setMethodName.append("(");
+        setMethodName.append(value);
+        setMethodName.append(");");
+        setList.add(String.valueOf(setMethodName));
+    }
 }
